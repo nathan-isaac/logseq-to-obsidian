1ef0bfa8bde096b4bcab4643a090f91d2c86dacd
Nathan Isaac <jarvis.git@pm.me>
2025-02-02 19:00:43 -0800

    feat: add app high level functions

--- main.go
+++ main.go
@@ -2,29 +2,65 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"path/filepath"
 )
 
 func main() {
+	app := New()
+	app.AddFlagConfig()
+
+	log.Printf("Logseq Directory: %s\n", app.LogseqDir)
+	log.Printf("Obsidian Directory: %s\n", app.ObsidianDir)
+
+	app.Sync()
+}
+
+type App struct {
+	LogseqDir   string
+	ObsidianDir string
+}
+
+func New() *App {
+	return &App{}
+}
+
+func (a *App) AddFlagConfig() {
 	logseqDir := flag.String("logseq-dir", "", "Path to the Logseq directory")
 	obsidianDir := flag.String("obsidian-dir", "", "Path to the Obsidian directory")
 
 	flag.Parse()
 
 	absLogseqDir, err := filepath.Abs(*logseqDir)
-
 	if err != nil {
 		log.Fatalf("Error geting absolute path for Logseq directory: %v", err)
 	}
+	a.LogseqDir = absLogseqDir
 
 	absObsidianDir, err := filepath.Abs(*obsidianDir)
-
 	if err != nil {
 		log.Fatalf("Error geting absolute path for Obsidian directory: %v", err)
 	}
+	a.ObsidianDir = absObsidianDir
+}
 
-	fmt.Printf("Logseq Directory: %s\n", absLogseqDir)
-	fmt.Printf("Obsidian Directory: %s\n", absObsidianDir)
+func (a *App) Sync() {
+	a.SyncAssets()
+	a.SyncJournal()
+	a.SyncPages()
+}
+
+func (a *App) SyncAssets() {
+	log.Println("Syncing assets")
+
+}
+
+func (a *App) SyncJournal() {
+	log.Println("Syncing journal")
+
+}
+
+func (a *App) SyncPages() {
+	log.Println("Syncing pages")
+
 }
